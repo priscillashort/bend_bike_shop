916351cc939a71335708f5d817cdaa1aa80e6537
Marji Symonds <symondsm@oregonstate.edu>
2020-02-17 11:53:05 -0800

    Add user input for customer name
    References #1

--- renting_bike_workflow.rb
+++ renting_bike_workflow.rb
@@ -1,3 +1,5 @@
+require "customer"
+
 class BikeRentingWorkflow
 
 	def initialize
@@ -6,7 +8,13 @@ class BikeRentingWorkflow
 
 
 	def run
+		puts "What is your first name? "
+		first_name = gets
 
+		puts "What is your last name? "
+		last_name = gets
+
+		customer = Customer.new(first_name, last_name)
 	end
 
 end
