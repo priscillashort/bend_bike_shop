db5e1541589909568fc51f780563b9327ffd3b3f
MaxTheMitchell <rat2043@gmail.com>
2020-02-19 11:14:19 -0800

    add time converstion funcs Issue #2

--- time_frame.rb
+++ time_frame.rb
@@ -1,4 +1,4 @@
-class Time_frame
+class TimeFrame
 
     attr_reader :start_time, :end_time, :start_date, :end_date
 
@@ -8,4 +8,27 @@ class Time_frame
         @start_time = start_time
         @end_time = end_time
     end
+
+    def get_time_diff(actual_return_time)
+        return self.time_to_minutes(actual_return_time - self.end_time)
+    end
+
+    def convert_to_miltary(time)
+        hours,minutes = time.split(':')
+        hours = hours.to_i
+        am_or_pm = minutes[/\D+/].upcase
+        hours -= 12 if hours == 12
+        hours += 12 if am_or_pm == "PM"
+        return hours,minutes[/\d+/].to_i
+    end
+
+    def time_to_minutes(time)
+        hours,minutes = convert_to_miltary(time) 
+        hours*60 + minutes
+    end
+
 end
+
+
+# main
+puts TimeFrame.new(1,1,1,1).time_to_minutes("12:15PM")
