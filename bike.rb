603eb78b6ccad020603dc09d0e3697f7207f2d25
MaxTheMitchell <rat2043@gmail.com>
2020-02-18 20:04:39 -0800

    create customer class Issue #2

---
+++ bike.rb
@@ -0,0 +1,11 @@
+class Bike 
+
+  attr_reader :weight, :model
+  
+  def initialize(weight,model)
+    @weight = weight
+    @model = model
+  end
+  
+    
+end
---
+++ customer.rb
@@ -0,0 +1,11 @@
+class Customer
+
+    def initialize(first_name,last_name,email,phone_numb,card_numb)
+        @first_name = first_name
+        @last_name = last_name
+        @email = email
+        @phone_numb = phone_numb
+        @card_numb = card_numb
+    end
+    
+end
