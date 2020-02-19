cd089ce04caa1ea8296e15f590d301215dcf326b
priscillashort <pshort2@cocc.edu>
2020-02-19 11:38:14 -0800

    Move promts into driver/main
    references #1

--- renting_bike_workflow.rb
+++ renting_bike_workflow.rb
@@ -1,56 +1,17 @@
-require_relative "customer"
-require_relative "time_frame"
-require_relative "bike"
 require_relative "Rental"
 
 class BikeRentingWorkflow
 
-	def initialize
+  attr_accessor :customer, :time_frame, :bike 
 
+	def initialize(customer, time_frame, bike)
+    @customer = customer
+    @time_frame = time_frame 
+    @bike = bike
 	end
 
-
 	def run
-		puts "What is your first name? "
-		first_name = gets.chomp
-
-		puts "What is your last name? "
-		last_name = gets.chomp
-
-		customer = Customer.new(first_name, last_name)
-
-		puts "What date do you want to start your rental?"
-		start_date = gets.chomp
-
-		puts "What date do you want to end your rental?"
-		end_date = gets.chomp
-
-		puts "What time do you want to start your rental?"
-		start_time = gets.chomp
-
-		puts "What time do you want to end yoor rental?"
-		end_time = gets.chomp
-
-		rental_time = TimeFrame.new(start_date, end_date, start_time, end_time)
-
-		puts "What model of bike do you want?"
-		bike_model = gets.chomp
-
-		rental_bike = Bike.new(1, bike_model, 50)
-
-		rental = Rental.new(customer, rental_time, rental_bike)
-
-		puts "Rental information:"
-		puts rental.to_s
-		puts "Confirm rental, y or n?"
-		confirm = gets.chomp
-
-		if confirm == 'y'
-			puts "Rental confirmed"
-		else
-			puts "Rental cancelled"
-		end
-
+	  rental = Rental.new(self.customer, self.time_frame, self.bike)
 	end
 
 end
