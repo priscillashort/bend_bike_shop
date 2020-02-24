08090388b86427d59f2dbf93437c02217998bf6a
priscillashort <pshort2@cocc.edu>
2020-02-24 11:23:33 -0800

    Add basic cancelation implementaion
    references #3

---
+++ cancel_rental.rb
@@ -0,0 +1,9 @@
+require_relative "cancel_rental_workflow"
+
+cancel_rental_workflow = CancelBikeWorkFlow.new
+
+cancel_rental_workflow.run
+
+
+
+
---
+++ cancel_rental_workflow.rb
@@ -0,0 +1,51 @@
+require_relative "bike"
+require_relative "customer"
+require_relative "rental"
+require_relative "time_frame"
+
+
+class CancelBikeWorkFlow
+  
+  attr_accessor :rentals
+
+  def initialize
+    @rentals = [
+      Rental.new(
+        Bike.new(5,:mountain),
+        Customer.new("Joe","Shmow","JoeTheShmow@fake.com",1234567890,555444333),
+        TimeFrame.new("02/18/20","02/18/20","11:30AM","3:45PM")
+      ),
+      Rental.new(
+        Bike.new(10,:road),
+        Customer.new("Moe","Shmow","MoeTheShmow@fake.com",1234567890,555444333),
+        TimeFrame.new("02/18/20","02/20/20","1:00AM","10:30AM")
+      ),
+      Rental.new(
+        Bike.new(15,:tricycle),
+        Customer.new("Poe","Shmow","PoeTheShmow@fake.com",1234567890,555444333),
+        TimeFrame.new("02/18/20","02/19/20","12:15PM","4:00PM")
+      )
+    ]
+  end
+
+  def run
+    puts "Which rental would you like to cancel?"
+    self.rentals.each.with_index{|r,i|puts "#{i}.#{r}"}
+
+  	print "select rental numb:"
+  	rental = self.rentals[gets().to_i]
+  	puts "you have selected #{rental}"
+
+  	puts "Are you sure you want to cancel this rental?"
+		puts "Confirm cancelation, y or n?"
+		confirm = gets.chomp
+
+		if confirm == 'y'
+    	puts "Cancelation confirmed"
+		else
+    	puts "Cancelation cancelled"
+		end
+
+  end
+
+end
