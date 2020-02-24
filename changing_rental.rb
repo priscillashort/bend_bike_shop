f118117779c0e17c72554ba8f768280eff43d355
Wyatt Pierce <piercewy@oregonstate.edu>
2020-02-24 11:53:48 -0800

    add new rental objects to change rental workflow
    References #4

--- changing_rental.rb
+++ changing_rental.rb
@@ -3,11 +3,24 @@ require_relative "customer"
 require_relative "time_frame"
 require_relative "bike"
 
-puts "What is your first name? "
-first_name = gets.chomp
+Rental.new(
+  Bike.new(5,:mountain),
+  Customer.new("Joe","Shmow","JoeTheShmow@fake.com",1234567890,555444333),
+  TimeFrame.new("02/18/20","02/18/20","11:30AM","3:45PM")
+),
+Rental.new(
+  Bike.new(10,:road),
+  Customer.new("Moe","Shmow","MoeTheShmow@fake.com",1234567890,555444333),
+  TimeFrame.new("02/18/20","02/20/20","1:00AM","10:30AM")
+),
+Rental.new(
+  Bike.new(15,:tricycle),
+  Customer.new("Poe","Shmow","PoeTheShmow@fake.com",1234567890,555444333),
+  TimeFrame.new("02/18/20","02/19/20","12:15PM","4:00PM")
+)
 
-puts "What is your last name? "
-last_name = gets.chomp
+puts "Enter Confirmation Code: "
+confirmation_code = gets.chomp
 
 
 
