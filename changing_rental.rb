ba13a5aa11a28642b37f2700decf9bf3d6f2dc84
Marji Symonds <symondsm@oregonstate.edu>
2020-02-26 10:31:11 -0800

    Add array of bike objects and confirmation search
    References #4

--- changing_rental.rb
+++ changing_rental.rb
@@ -3,7 +3,7 @@ require_relative "customer"
 require_relative "time_frame"
 require_relative "bike"
 
-Rental.new(
+bikes = [nil, Rental.new(
   Bike.new(5,:mountain),
   Customer.new("Joe","Shmow","JoeTheShmow@fake.com",1234567890,555444333),
   TimeFrame.new("02/18/20","02/18/20","11:30AM","3:45PM")
@@ -17,13 +17,13 @@ Rental.new(
   Bike.new(15,:tricycle),
   Customer.new("Poe","Shmow","PoeTheShmow@fake.com",1234567890,555444333),
   TimeFrame.new("02/18/20","02/19/20","12:15PM","4:00PM")
-)
+)]
 
 puts "Enter Confirmation Code: "
 confirmation_code = gets.chomp
 
-
+rental_match = bikes[confirmation_code]
 
 puts "Rental information:"
-puts rental.to_s
+puts rental_match.to_s
 
