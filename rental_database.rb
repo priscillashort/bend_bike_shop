210cea1012cad7ac65a97b7405689a938995f4d1
MaxTheMitchell <rat2043@gmail.com>
2020-02-27 14:30:23 -0800

    add more fake rentals to database
    refrences #3

--- rental_database.rb
+++ rental_database.rb
@@ -17,21 +17,32 @@ class RentalDatabase
       Rental.new(
         Bike.new(5,:mountain),
         Customer.new("Joe","Shmow","JoeTheShmow@fake.com",1234567890,555444333),
-        TimeFrame.new("02/26/20","03/18/20","11:30AM","3:45PM")
+        TimeFrame.new("02/27/20","02/27/20","11:30AM","3:45PM")
       ),
       Rental.new(
         Bike.new(10,:road),
         Customer.new("Moe","Shmow","MoeTheShmow@fake.com",1234567890,555444333),
-        TimeFrame.new("02/26/20","03/20/20","4:00PM","10:30AM")
+        TimeFrame.new("03/10/20","03/20/20","4:00PM","10:30AM")
       ),
       Rental.new(
         Bike.new(15,:tricycle),
         Customer.new("Poe","Shmow","PoeTheShmow@fake.com",1234567890,555444333),
         TimeFrame.new("02/27/20","03/19/20","12:15PM","4:00PM")
+			),
+			Rental.new(
+        Bike.new(15,:road),
+        Customer.new("Poe","Shmow","PoeTheShmow@fake.com",1234567890,555444333),
+        TimeFrame.new("03/1/20","03/2/20","12:15PM","4:00PM")
+			),
+			Rental.new(
+        Bike.new(15,:mountain),
+        Customer.new("Poe","Shmow","PoeTheShmow@fake.com",1234567890,555444333),
+        TimeFrame.new("02/29/20","03/01/20","12:15PM","4:00PM")
       )
     	]
 		)
 	end
+
 
 	def get_rental(conf_code)
 		rentals.each{|r| return r if r.confirmation_code==conf_code}
