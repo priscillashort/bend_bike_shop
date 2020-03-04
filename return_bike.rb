f1673d8d3c55aa40143fbd579f23f33dd348db48
Marji Symonds <symondsm@oregonstate.edu>
2020-03-04 11:21:18 -0800

    Move all driver code out of return_bike_workflow
    References #7

--- return_bike.rb
+++ return_bike.rb
@@ -1,8 +1,29 @@
 require_relative "return_bike_workflow"
 require_relative "rental_database"
 
-rental_database = RentalDatabase.fake
+rental_db = RentalDatabase.fake
 
-workflow = ReturnBikeWorkFlow.new(rental_database)
+workflow = ReturnBikeWorkFlow.new(rental_db)
 
-workflow.run
+puts "Which rental would you like to return?"
+puts rental_db.rentals
+
+print "\nselect rental confirmation number:"
+rental = rental_db.get_rental(gets.to_i)
+puts "\nyou have selected #{rental}"
+
+puts "\nWhat time did they return it?"
+print "Enter time (ex: 3:45PM, 8:15AM): "
+return_time = gets
+print "Enter date (ex: mm/dd/yy, 02/28/20): "
+date = gets
+
+print "\nEnter a description of the condition of the bike(s): "
+condition_description = gets
+
+workflow.run(date, return_time, rental)
+puts "\n#{rental.customer} has $#{rental.late_fee} in late fees"
+puts "Total price: $#{rental.bike.price}"
+puts "Including $#{rental.late_fee} in late fees" if rental.is_late
+puts "#{rental.customer}'s was charged on card with number: #{rental.payment_info}"
+
