b0e9892f71001652eaff7465422465cab8f227fb
MaxTheMitchell <rat2043@gmail.com>
2020-03-05 16:21:56 -0800

    add weight prompt to add to inventory workflow

--- add_rentable_to_inventory_workflow.rb
+++ add_rentable_to_inventory_workflow.rb
@@ -9,11 +9,15 @@ class AddRentableToInventoryWorkflow
   def run
     puts "Enter what kind of item you want to add:\nBike\nKayak"
     rentable_type = gets.downcase.strip
+    puts "enter weight:"
+    weight = gets.to_i
     puts "enter the model:"
     if rentable_type == 'bike'
-      inventory.add_item(Bike.new(3,gets.strip))
+      inventory.add_item(Bike.new(weight,gets.strip))
+      puts "successful entry!"
     elsif rentable_type == 'kayak'
-      inventory.add_item(Kayak.new(6,gets.strip))
+      inventory.add_item(Kayak.new(weight,gets.strip))
+      puts "successful entry!"
     else
       puts "invaild item type!"
     end
