1e1f0e701b259daea0bc16d4c3895019b519a689
Marji Symonds <symondsm@oregonstate.edu>
2020-03-08 13:06:33 -0700

    Use fake database for changing rental workflow
    References #7

--- changing_rental.rb
+++ changing_rental.rb
@@ -5,21 +5,7 @@ require_relative "time_frame"
 require_relative "rentable"
 require_relative "rental_database"
 
-rentals = [
-  Rental.new(
-    Bike.new(5,:mountain), 
-    Customer.new("Joe","Shmow","JoeTheShmow@fake.com",1234567890,555444333),
-    TimeFrame.new("02/18/20","02/18/20","11:30AM","3:45PM")),
-  Rental.new(
-    Bike.new(10,:road), 
-    Customer.new("Moe","Shmow","MoeTheShmow@fake.com",1234567890,555444333),
-    TimeFrame.new("02/18/20","02/20/20","1:00AM","10:30AM")),
-  Rental.new(
-    Bike.new(15,:tricycle), 
-    Customer.new("Poe","Shmow","PoeTheShmow@fake.com",1234567890,555444333),
-    TimeFrame.new("02/18/20","02/19/20","12:15PM","4:00PM"))]
-
-rental_db = RentalDatabase.new(rentals)
+rental_db = RentalDatabase.fake
 
 puts rental_db.rentals
 
